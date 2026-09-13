2196b23f4ea8b758b03c6753d5c66f56fb52c337
TaynaJimenes <tayna.jimenes@gmail.com>
2026-09-13 18:48:30 -0300

    Feat: Adicionando comentario em arquivo .ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from 'path'; //não sei se esse erro é normal
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
